b2704832b7a0bf6ba64bf27f92c43ad801775a62
Jonathan Phoon <jonathan@aleph-labs.com>
2016-11-17 21:19:07 +0800

    change mongo uri

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/eve'
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/eve'
 const mongoose = require('mongoose')
 const Event = require('./models/event')
 const CronJob = require('cron').CronJob
